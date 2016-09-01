a02cd0e49faa33ce07566eac60820c00ef9ab1d3
byuan <boyuan@Bos-iMac.local>
2016-09-01 00:22:15 -0400

    add problem 2 source

---
+++ 2_max_value_loot.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+double get_max_loot_value(const unsigned int &n, const unsigned long &W,
+					      const vector<unsigned int> &v, const vector<unsigned int> &w) {
+
+	static vector<double> unit_v(n);
+	for (int i=0; i < v.size();i++) {
+
+		unit_v[i] = v[i]/w[i];
+	}
+	
+}
+
+int main() {
+
+	unsigned int n;
+	unsigned long W;
+	vector<unsigned int> v;
+	vector<unsigned int> w;
+	cin >> n >> W;
+	for (int i=0;i<n;i++) 
+		cin >> v[i] >> w[i];
+
+	double out = get_max_loot_value(n, W, v, w);
+	cout.precision(5);
+	cout << out << endl;
+	return 0;
+
+
+	
+
+
+
+
+}
+
