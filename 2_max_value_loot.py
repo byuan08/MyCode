0cd7e322fc8aa46b397e232a8a37148ee1d2b8cd
byuan08 <byuan08@users.noreply.github.com>
2016-09-06 16:54:55 -0400

    Update 2_max_value_loot.py
    initial add of the main

---
+++ 2_max_value_loot.py
@@ -0,0 +1,19 @@
+import sys
+
+if __name__ == "main":
+  line1 = list(map(int, sys.stdin.readline().split()))
+  n = line1[0]
+  W = line1[1]
+  value = list()
+  weight = list()
+  for i in range[0:n]:
+    line = list(map(int, sys.stdin.readline().split()))
+    value.append(value[0])
+    weight.append(weight[0])
+    
+  
+    
+    
+  
+  
+  
