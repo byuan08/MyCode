96e21f3f2882f0c14aab794e20d4740f02397888
byuan08 <byuan08@users.noreply.github.com>
2016-09-07 13:06:40 -0400

    Update 3_max_ad_revenue.py

--- 3_max_ad_revenue.py
+++ 3_max_ad_revenue.py
@@ -1,4 +1,16 @@
 import os
 
+def dot_product(a, b):
+  if len(a) > 0:
+    return a.pop() * b.pop() + dot_product(a,b)
+  else:
+    return 0
+
 if __name__ == "__main__":
+  n = int(os.stdin.readline())
+  profit_per_click = list(map(int, os.stdin.readline().split())).sort()
+  clicks_per_day = list(map(int, os.stdin.readline().split())).sort()
+  out = dot_product(a, b)
+  print(out)
+  
   
