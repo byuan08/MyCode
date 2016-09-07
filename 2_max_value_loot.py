23653cb636d8e2e6aec188a6fff836df4e76b291
byuan08 <byuan08@users.noreply.github.com>
2016-09-07 10:37:03 -0400

    Update 2_max_value_loot.py

--- 2_max_value_loot.py
+++ 2_max_value_loot.py
@@ -1,18 +1,47 @@
 import sys
 
-if __name__ == "main":
-  line1 = list(map(int, sys.stdin.readline().split()))
-  n = line1[0]
-  W = line1[1]
-  value = list()
-  weight = list()
-  for i in range[0:n]:
-    line = list(map(int, sys.stdin.readline().split()))
-    value.append(value[0])
-    weight.append(weight[0])
+def get_max_loot_value(Capacity, item_sorted):
+	item = item_sorted.pop()
+	w = item[0]
+	unit_value = item[1]
+	print("w is: ", w)
+	print("uvalue is: ", unit_value)
+	if w <= Capacity:
+		Capacity = Capacity - w
+		#item_sorted.pop()
+		return w * unit_value + get_max_loot_value(Capacity, item_sorted)
+
+	else:
+
+		return Capacity * unit_value
+
+
+
+#if __name__ == "__main__":
+print('running main')
+line1 = list(map(int, sys.stdin.readline().split()))
+n = line1[0]
+W = line1[1]
+print(W)
+value = list()
+items = list()
+
+for i in range(n):
+	line = list(map(int, sys.stdin.readline().split()))
+	value = line[0]
+	weight = line[1]
+	unit_value = float(value/weight)
+	items.append((weight, unit_value))
+
+items.sort(key = lambda tup: tup[1])
+print(items)
+
+print(get_max_loot_value(W, items))
+
+
+
     
   
-    
     
   
   
