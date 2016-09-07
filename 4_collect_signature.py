6ebcfbf268542025e6a0ab655b8fc06917a8a308
byuan08 <byuan08@users.noreply.github.com>
2016-09-07 17:42:51 -0400

    Update 4_collect_signature.py

--- 4_collect_signature.py
+++ 4_collect_signature.py
@@ -2,6 +2,17 @@ import sys
 
 def get_min_points_to_cover_segments(n, segments):
   if len(segments) > 0:
+    endpoint_cur = segments.pop()[1]
+    # Only check the points whose start point is smaller than the current end point
+    i = 0
+    for x in range(1:n-1)
+      count++
+      position.append(endpoint_cur)
+      i++
+    
+    n = n-i
+      
+      
     
 if __name__ == "__main__":
   n = int(sys.stdin.readline())
@@ -14,7 +25,9 @@ if __name__ == "__main__":
     segs.append(seg)
   # sort the list of points by the endpoint
   segs.sort(key = lambda tup:tup[1])
-  
+  # create global variables to store the results
+  global count = 0
+  global position = list()
   m, position = get_min_points_to_cover_segments(n, segs)
   print(m)
   for x in position:
