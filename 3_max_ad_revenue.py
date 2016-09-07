a87a91ddced53061ed052443c4816d332180dc08
byuan08 <byuan08@users.noreply.github.com>
2016-09-07 13:31:02 -0400

    Add shebang. Split sort() to new line.

--- 3_max_ad_revenue.py
+++ 3_max_ad_revenue.py
@@ -1,16 +1,19 @@
-import os
+#! python3
+import sys
 
 def dot_product(a, b):
-  if len(a) > 0:
-    return a.pop() * b.pop() + dot_product(a,b)
-  else:
-    return 0
+	if len(a) > 0:
+		return a.pop() * b.pop() + dot_product(a,b)
+	else:
+		return 0
 
 if __name__ == "__main__":
-  n = int(os.stdin.readline())
-  profit_per_click = list(map(int, os.stdin.readline().split())).sort()
-  clicks_per_day = list(map(int, os.stdin.readline().split())).sort()
-  out = dot_product(a, b)
-  print(out)
-  
-  
+	#print('main running')
+
+	n = int(sys.stdin.readline())
+	profit_per_click = list(map(int, sys.stdin.readline().split()))
+	profit_per_click.sort()
+	clicks_per_day = list(map(int, sys.stdin.readline().split()))
+	clicks_per_day.sort()
+	out = dot_product(profit_per_click, clicks_per_day)
+	print(out)
