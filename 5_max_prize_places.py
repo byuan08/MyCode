7d515df176dc7812e1ee9dd64c05472151e7469b
byuan <byuan.scut@gmail.com>
2016-09-12 11:53:58 -0400

    test 5

--- 5_max_prize_places.py
+++ 5_max_prize_places.py
@@ -7,9 +7,9 @@ def get_max_number_of_summands(n, l):
 	if n < l:
 		print('The input has to be interger bigger than 0')
 		return []
-		#print("case 1")
+		print("case 1")
 
-	while ():
+	while (True):
 		if n > 2*l:
 			summands.append(l)
 			n -= l
@@ -17,15 +17,16 @@ def get_max_number_of_summands(n, l):
 			#print ("case 3,  n = {}, l = {}, summands = {}".format(n, l, summands))
 			continue
 			
-		
-		if n <= 2 * l:
+		else:
 			summands.append(n)
 			#print("case 2,  n = {}, l = {}, summands = {}".format(n, l, summands))
 			return summands
 	
 if __name__ == "__main__":
+	print('running main')
 	n = int(sys.stdin.readline())
+	print("n= ",n)
 	summands = get_max_number_of_summands(n, 1)
 	print(len(summands))
 	for x in summands:
-		print(x, end=' ')
+		#print(x, end=' ')
